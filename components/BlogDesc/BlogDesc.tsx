e8c470d27471d12d70fc99b59e792b75ad18126f
RevazSologhashvili <115929103+RevazSologhashvili@users.noreply.github.com>
2024-01-08 13:34:43 +0400

    Update BlogDesc.tsx

--- components/BlogDesc/BlogDesc.tsx
+++ components/BlogDesc/BlogDesc.tsx
@@ -14,10 +14,10 @@ const BlogDesc = (props: blogDescription) => {
 
   return (
     <div className="flex flex-col gap-3">
-      <div className="bg-[#F4D04E] w-fit px-3 py-1 rounded font-[800] text-[#111111]">
+      <div className="bg-[#F4D04E] w-fit px-3 py-1 rounded font-[800] text-[#111111] sm:text-[14px] text-[12px]">
         {props.blogCategory}
       </div>
-      <span className="text-[#111111] text-sm font-[500] ">
+      <span className="text-[#111111] text-sm font-[500] sm:text-[14px] text-[12px]" >
       {props.publishedDate}
       </span>
       <h2 className="sm:text-[24px] text-[20px] font-[800] w-fit hover:text-[#F4D04E] hover:cursor-pointer" onMouseLeave={()=> props.gotReleased()} onMouseOver={()=> props.gotHoverd()}>{props.blogHeading}</h2>
